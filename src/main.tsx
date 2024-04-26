e10035caf563725c11c2c67bda12373949b92872
ilay goldberg <ilay.gold123@gmail.com>
2024-04-26 14:53:57 +0300

    chore: clean unnecessery files

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-
-import { invoke } from '@tauri-apps/api'
-
-// now we can call our Command!
-// Right-click the application background and open the developer tools.
-// You will see "Hello, World!" printed in the console!
-invoke('greet', { name: 'World' })
-  // `invoke` returns a Promise
-  .then((response) => console.log(response))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
